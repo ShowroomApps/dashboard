dbd361ed5f52b01ea7f205a3f817f504c92e57e8
Muhammad Mustafa <mustafa@loadsecure.com>
2026-08-01 05:47:23 +0500

    chore: update API destination to use environment variable for backend URL

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/api/v1/:path*',
-        destination: 'http://localhost:3001/api/v1/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ];
   },
